7e8a987b19184c588602a6f512d10ce051e8e08e
seoyoungnation <ourworlds19@gmail.com>
2019-11-01 15:02:25 +0900

    add lab 11

---
+++ 20191101/array.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#define SIZE 5
+
+void print_array(int*);
+void sum_array(int*, int*);
+
+int main() {
+	int sum = 0;
+	int arr[5] = {2, 4, 6, 8, 10};
+	
+	print_array(arr);
+	sum_array(arr, &sum);
+
+	printf("sum = %d\n", sum);
+}
+
+void print_array(int* arr){
+	for (int i = 0; i < SIZE; i++)
+		printf("%d ", arr[i]);
+	printf("\n");
+	return;
+}
+
+void sum_array(int* arr, int* psum){
+	for (int i = 0; i < SIZE; i++) 
+		*psum += arr[i];
+}
+	
