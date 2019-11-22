e03a6f88ea516075a426b03e4a82d8ad938aa902
seoyoungnation <ourworlds19@gmail.com>
2019-11-22 15:08:31 +0900

    add ch15

---
+++ id.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+
+int main() {
+	int year, month, day;
+	printf("Input your first six numbers of your id: ");
+	scanf("%2d%2d%2d", &year, &month, &day);
+	printf("year = %d, month = %d, day = %d\n", year, month, day);
+	}
